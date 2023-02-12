90d96d4b3d12b38e0bcbd84f57b7eaa4b2b737c1
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-02-12 18:17:45 +0100

    models arrange

--- console.py
+++ console.py
@@ -10,12 +10,12 @@ from datetime import datetime
 import cmd
 import models
 from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
 from models.amenity import Amenity
+from models.city import City
 from models.place import Place
 from models.review import Review
+from models.state import State
+from models.user import User
 import re
 import shlex
 
